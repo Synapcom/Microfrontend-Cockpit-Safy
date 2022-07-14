ca666a0904c2a6a7c7c5dce34c9d8b142d005241
GustavoMeloInfraCommerce <gustavo.melo@infracommerce.com.br>
2022-07-14 16:53:18 -0300

    :recycle: removing hard code and commentaries to implement data mock

--- src/components/transactionDetails/ClientInformation/index.tsx
+++ src/components/transactionDetails/ClientInformation/index.tsx
@@ -2,99 +2,100 @@ import React from 'react';
 import Container from './style';
 import ClientInformationInterface from './interface';
 
-const ClientInformation: React.FC<ClientInformationInterface> = ({ address, client, shippingAddress }) => {
+const ClientInformation: React.FC<ClientInformationInterface> = ({ address, client, paymentAddress}) => {
     return (
         <Container>
             <article>
                 <section className='personalClientInformation'>
                     <h2>Cliente</h2>
-                    <p>Gustavo Melo</p>
+                    <p>{`${client.firstName} ${client.lastName}`}</p>
 
                     <h2>E-mail</h2>
-                    <p>gsantos15569@gmail.com</p>
+                    <p>{client.email}</p>
 
                     <div>
                         <span>
-                            <h2>CPF</h2>
-                            <p>123.456.789-00</p>
+                            <h2>{client.documentType}</h2>
+                            <p>{client.document}</p>
                         </span>
                         <span>
                             <h2>Telefone</h2>
-                            <p>11 9999999999</p>
+                            <p>{client.phone}</p>
                         </span>
                     </div>
                 </section>
                 <hr />
                 <section>
                     <h2>Endereço de cobrança</h2>
-                    <p>Rua das Palmeiras</p>
+                    <p>{paymentAddress.street}</p>
 
                     <div>
                         <span>
                             <h2>Numero</h2>
-                            <p>507</p>
+                            <p>{paymentAddress.number}</p>
                         </span>
                         <span>
                             <h2>Complemento</h2>
-                            <p>Casa</p>
+                            <p>{paymentAddress.complement}</p>
                         </span>
                     </div>
 
                     <div>
                         <span>
                             <h2>CEP</h2>
-                            <p>000000-01</p>
+                            <p>{paymentAddress.postalCode}</p>
                         </span>
                         <span>
                             <h2>Bairro</h2>
-                            <p>Palmeiras - Barra funda</p>
+                            <p>{paymentAddress.neighborhood}</p>
                         </span>
                     </div>
 
                     <div>
                         <span>
                             <h2>Cidade</h2>
-                            <p>São Paulo</p>
+                            <p>{paymentAddress.city}</p>
                         </span>
                         <span>
                             <h2>Estado</h2>
-                            <p>São Paulo</p>
+                            <p>{paymentAddress.state}</p>
                         </span>
                     </div>
                 </section>
                 <section>
                     <h2>Endereço de entrega</h2>
-                    <p>Rua das Palmeiras</p>
+                    <p>{address.street}</p>
+
                     <div>
                         <span>
                             <h2>Numero</h2>
-                            <p>507</p>
+                            <p>{address.number}</p>
                         </span>
                         <span>
                             <h2>Complemento</h2>
-                            <p>Casa</p>
+                            <p>{address.complement}</p>
                         </span>
                     </div>
 
                     <div>
                         <span>
                             <h2>CEP</h2>
-                            <p>0000000-11</p>
+                            <p>{address.postalCode}</p>
                         </span>
                         <span>
                             <h2>Bairro</h2>
-                            <p>Palmeiras - Barra funda</p>
+                            <p>{address.neighborhood}</p>
                         </span>
                     </div>
 
                     <div>
                         <span>
                             <h2>Cidade</h2>
-                            <p>São Paulo</p>
+                            <p>{address.city}</p>
                         </span>
                         <span>
                             <h2>Estado</h2>
-                            <p>São Paulo</p>
+                            <p>{address.state}</p>
                         </span>
                     </div>
                 </section>
